b43eb6e1d0b82034feb7051dda13c788c14db0f5
Myo Wai Yan Kyaw <myowaiyankyaw@hotmail.com>
2016-10-22 19:01:34 +0800

    People Bean

--- week03_ca/src/java/ejava2016/pt08/ca1/business/PeopleBean.java
+++ week03_ca/src/java/ejava2016/pt08/ca1/business/PeopleBean.java
@@ -6,13 +6,20 @@
 
 package ejava2016.pt08.ca1.business;
 
+import ejava2016.pt08.ca1.model.People;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
- * @author kyawminthu
+ * @author Myo Wai Yan Kyaw
  */
 @Stateless
 public class PeopleBean {
+    @PersistenceContext private EntityManager em;
     
+    public void register(People people) {
+        em.persist(people);
+    }
 }
