d1c59c2c6aef2ee95237d4ccb02a8bb81f1b9596
Zaw Min Thant <zawminthantt@gmail.com>
2016-10-22 19:55:48 +0800

    Added register method

--- week03_ca/src/java/ejava2016/pt08/ca1/rest/AppConfig.java
+++ week03_ca/src/java/ejava2016/pt08/ca1/rest/AppConfig.java
@@ -1,9 +1,15 @@
 package ejava2016.pt08.ca1.rest;
 
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class AppConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return super.getClasses(); //To change body of generated methods, choose Tools | Templates.
+    }
 	
 }
--- week03_ca/src/java/ejava2016/pt08/ca1/rest/PeopleResource.java
+++ week03_ca/src/java/ejava2016/pt08/ca1/rest/PeopleResource.java
@@ -8,13 +8,19 @@ package ejava2016.pt08.ca1.rest;
 import ejava2016.pt08.ca1.business.PeopleBean;
 import ejava2016.pt08.ca1.model.People;
 import java.util.Optional;
+import java.util.UUID;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 /**
@@ -41,4 +47,31 @@ public class PeopleResource {
 				.entity("Customer id not found:" + pID)
 				.build());
     }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response registerPeople(MultivaluedMap<String, String> formData) {
+        String name = formData.getFirst("name");
+        String email = formData.getFirst("email");
+        
+        People p = new People();
+        p.setName(name);
+        p.setEmail(email);
+        p.setPid(UUID.randomUUID().toString().substring(0, 8));
+        
+        peopleBean.register(p);
+        
+        JsonObject json = Json.createObjectBuilder()
+                .add("name", name)
+                .add("email", email)
+                .build();
+        
+        System.out.println(String.format(">>> Name: %s, Email: %s", name, email));
+        
+        return (Response.status(Response.Status.CREATED)
+                .entity(json.toString())
+                .build());
+    }
+    
 }
