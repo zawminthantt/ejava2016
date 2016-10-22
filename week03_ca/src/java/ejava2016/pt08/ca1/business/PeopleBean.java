ecb44c948a9ee783d9681867f8a26d5ecce58f06
Kyaw Min Thu <kyawminnthuu@gmail.com>
2016-10-22 18:13:23 +0800

    Add AppointmentBean, PeopleBean

---
+++ week03_ca/src/java/ejava2016/pt08/ca1/business/PeopleBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejava2016.pt08.ca1.business;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author kyawminthu
+ */
+@Stateless
+public class PeopleBean {
+    
+}
