acd0c0d41f590a810476ed70e884f29b4c999a51
Kyaw Min Thu <kyawminnthuu@gmail.com>
2016-10-22 18:11:51 +0800

    Commit PeopleResource plan.

---
+++ week03_ca/src/java/ejava2016/pt08/ca1/rest/AppConfig.java
@@ -0,0 +1,9 @@
+package ejava2016.pt08.ca1.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AppConfig extends Application {
+	
+}
---
+++ week03_ca/src/java/ejava2016/pt08/ca1/rest/PeopleResource.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejava2016.pt08.ca1.rest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author kyawminthu
+ */
+@RequestScoped
+@Path("/people")
+public class PeopleResource {
+    
+}
