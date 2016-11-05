6b07dacbe991db49f0193d735b931ba290f9c0be
Kyaw Min Thu <kyawminnthuu@gmail.com>
2016-11-05 19:25:26 +0800

    Commit ca2 login.

--- ca2/src/java/com/team08pt/LoginView.java
+++ ca2/src/java/com/team08pt/LoginView.java
@@ -5,10 +5,64 @@
  */
 package com.team08pt;
 
-/**
- *
- * @author zawminthant
- */
-public class LoginView {
-    
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@ViewScoped
+@Named
+public class LoginView implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	private String password;
+	private int count;
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+	public String login() {
+
+		HttpServletRequest req = (HttpServletRequest)FacesContext.getCurrentInstance()
+				.getExternalContext()
+				.getRequest();
+
+		try {
+			req.login(username, password);
+		} catch (ServletException ex) {
+			FacesMessage msg = new FacesMessage("Incorrect login");
+			count++;
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			return ("");
+		}
+
+		return ("/secure/topsecret");
+
+	}
+
+	
 }
