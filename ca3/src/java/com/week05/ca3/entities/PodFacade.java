c680a1a30b29eb8947b521c26aa215a6b4ab837e
Myo Wai Yan Kyaw <myowaiyankyaw@hotmail.com>
2016-11-13 01:12:25 +0800

    Entity

---
+++ ca3/src/java/com/week05/ca3/entities/PodFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.week05.ca3.entities;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Family-PC
+ */
+@Stateless
+public class PodFacade extends AbstractFacade<Pod> {
+
+    @PersistenceContext(unitName = "ca3PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PodFacade() {
+        super(Pod.class);
+    }
+    
+}
